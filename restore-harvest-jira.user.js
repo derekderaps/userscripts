949082679e805b07fe20fc05c78d9c0de71f8605
Derek DeRaps <derekderaps@users.noreply.github.com>
2017-06-28 23:47:29 +0100

    Initial commit for Restore Harvest JIRA

---
+++ restore-harvest-jira.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Move Harvest plugin to top of JIRA sidebar
+// @namespace    tenderwolf
+// @version      0.1
+// @description  Moves the Harvest time tracking plugin to top of JIRA sidebar, where it used to be!
+// @match        https://*.atlassian.net/*
+// @grant        none
+// ==/UserScript==
+
+// ==OpenUserJS==
+// @author derekderaps
+// ==/OpenUserJS==
+
+(function($) {
+    'use strict';
+
+    $('#com\\.strategery\\.plugins\\.harvest\\.jira-harvest-tracker__harvest-time-tracker').prependTo('#viewissuesidebar');
+})(jQuery);
