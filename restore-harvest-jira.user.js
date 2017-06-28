ae25256ea648af39b2be2ca28df4e2b0f4f9872a
Derek DeRaps <derekderaps@users.noreply.github.com>
2017-06-28 23:54:18 +0100

    Bump version

--- restore-harvest-jira.user.js
+++ restore-harvest-jira.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Move Harvest plugin to top of JIRA sidebar
 // @namespace    tenderwolf
-// @version      0.1
+// @version      0.2
 // @description  Moves the Harvest time tracking plugin to top of JIRA sidebar, where it used to be!
 // @author       Derek "Hawkeye Tenderwolf" DeRaps
 // @match        https://*.atlassian.net/*
